de524cd67c84556fb94ce7db8d0e6919064a08ff
ArashAzma <arashazma3@gmail.com>
2024-12-15 10:07:15 +0330

    test

--- utils/user.c
+++ utils/user.c
@@ -25,33 +25,9 @@ userInfo* get_user_input(users* users_list) {
     char priceThresholdInput[PRICE_THRESHOLD_INPUT];
     userInfo* user;
 
-    get_user_input_graphic(orderList, userID, priceThresholdInput);
-    printf("***Username: %s\n", userID);
-        for (int i = 0; i < users_list->user_count; i++) {
-        if (strcmp(users_list->users[i].userID, userID) == 0) {
-            user_founded = 1;
-            user = &users_list->users[i];
-        }
-    }
-    if (!user_founded) {
-        user = &users_list->users[users_list->user_count];
-        user->store1_order_count = 0;
-        user->store2_order_count = 1;
-        user->store3_order_count = 0;
-        users_list->user_count++;
-    };
-
-    if (strcmp(priceThresholdInput, "") == 0) priceThreshold = -1;
-    else priceThreshold = atoi(priceThresholdInput);
-    printf("Price threshold: %s %d\n", priceThresholdInput, priceThreshold);
-    printf("OrderList: \n");
-    for (int i = 0; i < ORDER_COUNT; i++) 
-        printf("\t%s %d\n", orderList[i].name, orderList[i].count);
-
-
-    // strcpy(userID, "user1");
-
-    // for (int i = 0; i < users_list->user_count; i++) {
+    // get_user_input_graphic(orderList, userID, priceThresholdInput);
+    // printf("***Username: %s\n", userID);
+    //     for (int i = 0; i < users_list->user_count; i++) {
     //     if (strcmp(users_list->users[i].userID, userID) == 0) {
     //         user_founded = 1;
     //         user = &users_list->users[i];
@@ -65,16 +41,42 @@ userInfo* get_user_input(users* users_list) {
     //     users_list->user_count++;
     // };
 
-    // strcpy(orderList[0].name, "Jeans");
-    // orderList[0].count = 2;
-    
-    // strcpy(orderList[1].name, "T-shirt");
-    // orderList[1].count = 1;
+    // if (strcmp(priceThresholdInput, "") == 0) priceThreshold = -1;
+    // else priceThreshold = atoi(priceThresholdInput);
+    // printf("Price threshold: %s %d\n", priceThresholdInput, priceThreshold);
+    // printf("OrderList: \n");
+    // for (int i = 0; i < ORDER_COUNT; i++) 
+    //     printf("\t%s %d\n", orderList[i].name, orderList[i].count);
 
-    // strcpy(orderList[2].name, "Dress");
-    // orderList[2].count = 2;
+
+    strcpy(userID, "user1");
+
+    for (int i = 0; i < users_list->user_count; i++) {
+        if (strcmp(users_list->users[i].userID, userID) == 0) {
+            user_founded = 1;
+            user = &users_list->users[i];
+        }
+    }
+    if (!user_founded) {
+        user = &users_list->users[users_list->user_count];
+        user->store1_order_count = 0;
+        user->store2_order_count = 1;
+        user->store3_order_count = 0;
+        users_list->user_count++;
+    };
+
+    strcpy(orderList[0].name, "Blouse with Frills");
+    orderList[0].count = 2;
     
-    // priceThreshold = 2200;
+    strcpy(orderList[1].name, "Jeans");
+    orderList[1].count = 1;
+
+    strcpy(orderList[2].name, "Dress");
+    orderList[2].count = 2;
+    
+    priceThreshold = 2200;
+
+
 
     // printf("Username: ");
     // scanf("%s", userID);
